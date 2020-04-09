6c92648a5452a383e380c3689c1263aadc6262ed
Jeppe <jeppe.aerenlund@gmail.com>
2020-04-09 21:49:08 +0200

    Strings mix

--- src/Codewars.Solutions/Program.cs
+++ src/Codewars.Solutions/Program.cs
@@ -14,7 +14,7 @@ namespace Codewars.Solutions
 
         static void Run()
         {
-            var task = new TwiceLinear();
+            var task = new StringsMix();
             var result = task.Run();
 
             Console.WriteLine(task.Rank);
---
+++ src/Codewars.Solutions/Tasks/StringsMix.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Codewars.Solutions.Core;
+
+namespace Codewars.Solutions.Tasks
+{
+    /// <summary>
+    /// Strings Mix
+    /// Given two strings s1 and s2, we want to visualize how different the two strings are. We will only take into account the lowercase letters (a to z).
+    /// </summary>
+    public class StringsMix : ITask
+    {
+        public string Name => "Strings Mix";
+        public string Rank => "4 Kuy";
+        public string Link => "https://www.codewars.com/kata/5629db57620258aa9d000014";
+
+        public string Run()
+        {
+            var cases = new string[]
+            {
+                "A aaaa bb c",
+                "my&friend&Paul has heavy hats! &",
+            };
+
+            var cases2 = new string[] 
+            {
+                "& aaa bbb c d",
+                "my friend John has many many friends &"
+            };
+
+            var results = new StringBuilder();
+
+            for (int i = 0; i < cases.Length; i++)
+            {
+                var result = Mix(cases[i], cases2[i]);
+                results.Append($"{cases[i]} | {cases2[i]} -> {result} \n");   
+            }
+
+            return results.ToString();
+        }
+
+        private string Mix(string s1, string s2)
+        {
+            var dicS1 = s1.GroupBy(x => x)
+                .Where(x => x.Count() > 1)
+                .ToDictionary(k => k.Key, v => v.Count());
+            
+            var dicS2 = s2.GroupBy(x => x)
+                .Where(x => x.Count() > 1)
+                .ToDictionary(k => k.Key, v => v.Count());
+
+            var unique = (s1 + s2).Replace(" ", "").Where(char.IsLower).ToHashSet();
+            var combined = new List<(char c, char pfx, int cnt)>();
+
+            foreach(var c in unique) 
+            {
+                var hasS1 = dicS1.TryGetValue(c, out var cntS1);
+                var hasS2 = dicS2.TryGetValue(c, out var cntS2);
+
+                if (!hasS1 && !hasS2)
+                    continue;
+
+                var cnt = Math.Max(cntS1, cntS2);
+                var pfx = cntS1 > cntS2 ? '1' : '2';
+
+                if (cntS1 == cntS2) 
+                    pfx = '=';
+
+                combined.Add((c, pfx, cnt));
+            }
+
+            var ordered = combined.OrderByDescending(x => x.cnt)
+                .ThenBy(x => x.pfx)
+                .ThenBy(x => x.c);
+
+            var formats = ordered.Select(x => $"{x.pfx}:{new string(x.c, x.cnt)}");
+
+            return string.Join("/", formats);
+        }
+    }
+}
